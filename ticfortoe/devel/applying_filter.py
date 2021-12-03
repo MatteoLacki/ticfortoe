e4431fe8a2d47104d5a15972b59235901fe9144e
MatteoLacki <matteo.lacki@gmail.com>
2021-12-03 15:18:16 +0100

    rerun

--- ticfortoe/devel/applying_filter.py
+++ ticfortoe/devel/applying_filter.py
@@ -35,33 +35,35 @@ rawdata = OpenTIMS('/home/matteo/raw_data/majestix/M201203_013_Slot1-1_1_708.d')
 def MS1_TIC_composition_plot(
     rawdata,
     binned_data,
-    labels=["Singly Charged Ions", "++ [0,50)", "++ [50,100)", "++ [100,150)", "++ [150,200)", "++>= 200"],
+    labels=["++>= 200", "++ [150,200)", "++ [100,150)", "++ [50, 100)", "++ [0,50)", "Singly Charged Ions"],
     show=True
 ):
-    rt_sec = binned_data.bin_borders["retention_time"]
-    rt_min = rt_sec / 60.0
     bruker_intensity_min, _ = np.histogram(
         rawdata.frames["Time"][rawdata.ms1_frames-1],
-        bins=rt_sec,
+        bins=binned_data.bin_borders["retention_time"],
         weights=rawdata.frames["SummedIntensities"][rawdata.ms1_frames-1]
     )
+    rt_sec = binned_data.bin_centers["retention_time"]
+    rt_min = rt_sec / 60.0
 
+    intensities = [data_p.sum(axis=0),*data_pp]
+
+    intensities.reverse()
     plt.subplot(2, 1, 1)
-    plt.scatter(rt_min, bruker_intensity_min,
+    plt.scatter(
+        rt_min,
+        bruker_intensity_min,
         label="FULL TIC according to BRUKER"
     )
-    plt.stackplot(rt_min, 
-        [data_p.sum(axis=0),*data_pp], 
-        labels=labels)
+    plt.stackplot(rt_min, *intensities, labels=labels)
     plt.legend()
     plt.title("Comparison of TIC calculations. Multiply charged region defined as 'scan >= 970.114551 - 0.6924664 * mz' ")
     plt.xlabel("Retention Time [minutes]")
     plt.ylabel("% of TIC [per minute]")
 
+    percentages = intensities / np.sum(intensities, axis=0)
     plt.subplot(2, 1, 2)
-    plt.stackplot(rt_min, 
-        *(data_p.sum(axis=0)/data.sum(axis=0), data_pp/data.sum(axis=0)), 
-        labels=labels)
+    plt.stackplot(rt_min, *percentages, labels=labels)
     plt.legend()
     plt.title("Composition of TIC in %. Multiply charged region defined as 'scan >= 970.114551 - 0.6924664 * mz' ")
     plt.xlabel("Retention Time [minutes]")
